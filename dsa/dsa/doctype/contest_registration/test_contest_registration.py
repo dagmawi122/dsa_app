9b689c8320a25655469faf7eecbc81bc2ba1b077
Degaga-Emiru <degagaemiru@gmail.com>
2026-08-31 13:32:14 +0300

    test: fix contest registration test setup

--- dsa/dsa/doctype/contest_registration/test_contest_registration.py
+++ dsa/dsa/doctype/contest_registration/test_contest_registration.py
@@ -1,4 +1,5 @@
 import frappe
+
 from frappe.tests.utils import FrappeTestCase
 from frappe.utils import add_to_date, now_datetime
 
@@ -12,6 +13,26 @@ class TestContestRegistration(FrappeTestCase):
         if self.user == "Guest":
             self.user = "Administrator"
 
+    def create_problem(self):
+        """Create a valid DSAProblem for the test Contest."""
+
+        problem = frappe.get_doc({
+            "doctype": "DSAProblem",
+            "title": "Test Two Sum Problem",
+            "difficulty": "easy",
+            "description": "Find the sum of two numbers.",
+            "test_cases": [
+                {
+                    "custom_input": "1 2",
+                    "custom_expected_output": "3",
+                }
+            ],
+        })
+
+        problem.insert(ignore_permissions=True)
+
+        return problem
+
     def create_contest(self, start_offset_hours=-1, end_offset_hours=2):
         start = add_to_date(
             now_datetime(),
@@ -23,6 +44,10 @@ class TestContestRegistration(FrappeTestCase):
             hours=end_offset_hours,
         )
 
+        # Create a valid DSAProblem first
+        problem = self.create_problem()
+
+        # Create Contest with the required Contest Problem
         contest = frappe.get_doc(
             {
                 "doctype": "Contest",
@@ -30,6 +55,13 @@ class TestContestRegistration(FrappeTestCase):
                 "description": "Contest registration test",
                 "start_date": start,
                 "end_date": end,
+                "problems": [
+                    {
+                        "problem": problem.name,
+                        "order": 1,
+                        "points": 100,
+                    }
+                ],
             }
         )
 
@@ -40,7 +72,10 @@ class TestContestRegistration(FrappeTestCase):
     def test_contest_status_is_active(self):
         contest = self.create_contest()
 
-        self.assertEqual(contest.status, "Active")
+        self.assertEqual(
+            contest.status,
+            "Active",
+        )
 
     def test_registration_can_be_created(self):
         contest = self.create_contest()
@@ -56,7 +91,10 @@ class TestContestRegistration(FrappeTestCase):
 
         registration.insert(ignore_permissions=True)
 
-        self.assertEqual(registration.status, "Joined")
+        self.assertEqual(
+            registration.status,
+            "Joined",
+        )
 
     def test_duplicate_registration_is_rejected(self):
         contest = self.create_contest()
